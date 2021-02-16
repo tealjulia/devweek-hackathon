c4ada9d2e714d6efe7ebd65b8e3ff3070257f6fe
Teal Larson <larson.teal@gmail.com>
2021-02-15 20:37:20 -0800

    force dialog/permissions on every login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,6 +10,8 @@ router.get('/login', (req, res) => {
         response_type: 'code',
         client_id: keys.SPOTIFY_CLIENT_ID,
         redirect_uri: keys.SPOTIFY_REDIRECT_URI,
+        scope: 'playlist-modify-public playlist-modify-private user-read-email user-read-private',
+        show_dialog: true
     })}`);
 });
 
